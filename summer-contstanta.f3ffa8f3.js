9d5b69ec8ae17bc1443797d6679a854db7806d71
MykhailoGaber <mishagaber0710@gmail.com>
2026-08-31 19:30:47 +0000

    Deploying to gh-pages from @ MykhailoGaber/summer-contstanta@953968ad54b98c755027688fdece79f8a58c77f6 🚀

---
+++ summer-contstanta.f3ffa8f3.js
@@ -0,0 +1,2 @@
+document.querySelector(".button.mogged__btn").addEventListener("click",()=>{alert("Вас освятил небесный десант чайки!"),document.body.style.backgroundColor="white",document.body.style.color="black"});
+//# sourceMappingURL=summer-contstanta.f3ffa8f3.js.map
